663e9ffb79d0d9331f7ffd299b0a4be6d9e8400f
KarakayaMetin8787 <karakayametin8787@gmail.com>
2023-12-12 17:15:32 +0100

    grundstruktur erstellt

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <div>
-
+      <Header/>
+      <BrowserRouter>
+      <Routes>
+        <Route/>
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import Hero from "../Hero/Hero";
+import Nav from "../Nav/Nav";
+
+
+const Header = () => {
+
+
+
+    const SearchButton = () => {
+
+    }
+
+    return ( 
+        <>
+        <Nav/>
+        <Hero/>
+        </>
+    );
+}
+
+export default Header;
