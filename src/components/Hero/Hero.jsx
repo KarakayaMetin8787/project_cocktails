4034e2a2dfc6ab8d45851b4c45bd5738a4ed4638
KarakayaMetin8787 <karakayametin8787@gmail.com>
2023-12-13 14:08:48 +0100

    replaced <Link> with useNavigate within my fetch

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,11 @@
 import { useContext, useState } from "react";
 import { SearchFetchContext } from "../../Context/Context";
-import { Link } from "react-router-dom";
-import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Hero = () => {
     const [searchInput, setSearchInput] = useState("");
     const SearchNameValue = useContext(SearchFetchContext);
-    const location = useLocation()
+    const navigate = useNavigate();
     
     function handleSubmit(event) {
         event.preventDefault()
@@ -16,11 +15,10 @@ const Hero = () => {
             fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchInput}`)
                 .then (response => response.json())
                 .then(data => {
-                    const newLocation = { ...location, pathname: "/productlist" };
-                window.history.pushState(null, "", newLocation.pathname);
+                    SearchNameValue.setSearchName(data);
+                    navigate("/productlist");
                 })
                 .catch (error => console.error(error));
-
         }
     
     return ( 
@@ -28,7 +26,6 @@ const Hero = () => {
         <form onSubmit={handleSubmit}>
             <input type="text" onChange={(event) => setSearchInput(event.target.value)}/>
             <input type="submit" value="Search" onClick={() => SearchButton()}/>
-            <Link to="/productlist">go to productlist</Link>
         </form>
         </>
     );
