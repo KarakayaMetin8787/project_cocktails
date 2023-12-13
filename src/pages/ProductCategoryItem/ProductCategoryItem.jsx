2b016d199b884fa67ca20844d36aa8a354aebe35
SuperCoderSuat <supercodesuat@gmail.com>
2023-12-13 14:14:31 +0100

    update merge dev branch

--- src/pages/ProductCategoryItem/ProductCategoryItem.jsx
+++ src/pages/ProductCategoryItem/ProductCategoryItem.jsx
@@ -4,7 +4,7 @@ import "./ProductCategoryItem.scss";
 const ProductCategoryItem = ({ name, img, id }) => {
   return (
     <>
-      <Link to={`/products/${id}`}>
+      <Link to={`/products/product-details/${id}`}>
         <li className="listitem">
           <p className="card_title">{name}</p>
           <img src={img} alt="drink-img" />
