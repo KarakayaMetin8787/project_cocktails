631c3d3b10ec96e6ad5ffa21de9b16d3d179fa3c
KWiefel <dev.kwiefel427@gmail.com>
2023-12-13 16:11:46 +0100

    basic design added to productlist and child components

--- src/pages/ProductCategoryItem/ProductCategoryItem.jsx
+++ src/pages/ProductCategoryItem/ProductCategoryItem.jsx
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom";
 import "./ProductCategoryItem.scss";
+import moreBTN from "./../../../public/moreBTN.png";
 
 const ProductCategoryItem = ({ name, img, id }) => {
   return (
     <>
-      <Link to={`/products/${id}`}>
-        <li className="listitem">
-          <p className="card_title">{name}</p>
-          <img src={img} alt="drink-img" />
-        </li>
-      </Link>
+      <div className="gridbox">
+        <Link to={`/products/product-details/${id}`}>
+          <li className="listitem">
+            <div className="titlebox">
+              <p className="card_title">{name}</p>
+              <img src={moreBTN} alt="" />
+            </div>
+            <img src={img} alt="drink-img" />
+          </li>
+        </Link>
+      </div>
     </>
   );
 };
