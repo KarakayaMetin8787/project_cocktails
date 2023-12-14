a2a15f5bf90b6beaab1949f0192aba63e67345cf
KWiefel <dev.kwiefel427@gmail.com>
2023-12-14 10:39:13 +0100

    fixed design problems

--- src/App.jsx
+++ src/App.jsx
@@ -11,29 +11,32 @@ import ProductCategoryList from "./pages/ProductCategoryList/ProductCategoryList
 import CreateProduct from "./pages/CreateProduct/CreateProduct";
 import ProductDetails from "./pages/ProductDetails/ProductDetails";
 
-
 function App() {
-
-const [searchName, setSearchName] = useState([]);
-const [goBackURL, setgoBackURL] = useState("");
+  const [searchName, setSearchName] = useState([]);
+  const [goBackURL, setgoBackURL] = useState("");
 
   return (
-
     <BrowserRouter>
       <GoBackButtonContext.Provider value={{ goBackURL, setgoBackURL }}>
-      <SearchFetchContext.Provider value={{ searchName, setSearchName }}>
-      <div className="">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products/product-details/:id"element={<ProductDetails />} />
-          <Route path="/createProduct" element={<CreateProduct />} />
-          <Route path="/products/:category" element={<ProductCategoryList />} />
-          <Route path="/productlist" element={<ProductList />} />
-        </Routes>
-        <Footer />
-      </div>
-      </SearchFetchContext.Provider>
+        <SearchFetchContext.Provider value={{ searchName, setSearchName }}>
+          <div className="">
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route
+                path="/products/product-details/:id"
+                element={<ProductDetails />}
+              />
+              <Route path="/createProduct" element={<CreateProduct />} />
+              <Route
+                path="/products/:category"
+                element={<ProductCategoryList />}
+              />
+              <Route path="/productlist" element={<ProductList />} />
+            </Routes>
+            <Footer />
+          </div>
+        </SearchFetchContext.Provider>
       </GoBackButtonContext.Provider>
     </BrowserRouter>
   );
